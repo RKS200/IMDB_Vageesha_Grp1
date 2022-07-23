b7c089c443547e65ef23e74ad4f1558b11a66627
RKS2006 <63867778+RKS200@users.noreply.github.com>
2022-07-23 09:05:51 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -221,11 +221,11 @@ def searchbycast():
 
 def searchbyrating():
     global userid
-    rate = int(input('\n\n>>> Enter the Rating: '))
+    r = int(input('\n\n>>> Enter the Rating: '))
     l = load()
     a = 0
     for i in l:
-        if(int(rate(i)) == rate):
+        if(int(rate(i)) == r):
             a += 1
             print('{:<20}'.format("\nMovie Name: "),i['name'],'{:<20}'.format("\nYear: "),i['year'],'{:<20}'.format("\nRelease Date: "),i['date'],'{:<20}'.format("\nDuration: "),i['dur'])
             print('Casts:')
@@ -262,7 +262,7 @@ def toprate():
         a[rate(i)] = i
     b = sorted(a)
     print('\n\nTop Rated')
-    for i in b[:5]:
+    for i in sorted(b[:5],reverse = True):
         print('{:<20}{:<20}{:<20}'.format(a[i]['name'],i,a[i]['genre']))
         
 def bohits():
@@ -272,7 +272,7 @@ def bohits():
         a[i['boxoffice']] = i
     b = sorted(a)
     print('\n\nBox Office Hits')
-    for i in b[:5]:
+    for i in sorted(b[:5],reverse = True):
         print('{:<20}{:<20}{:<20}'.format(a[i]['name'],i,a[i]['genre']))
         
 def mostviewed():
@@ -282,7 +282,7 @@ def mostviewed():
         a[len(i['rate'])] = i
     b = sorted(a)
     print('\n\nBox Office Hits')
-    for i in b[:5]:
+    for i in sorted(b[:5],reverse = True):
         print('{:<20}{:<20}{:<20}'.format(a[i]['name'],i,a[i]['genre']))
         
 def surf():
